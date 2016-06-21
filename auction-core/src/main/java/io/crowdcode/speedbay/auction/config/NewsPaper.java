c7b977d8a58b2e57d6a81c01eedb1922fd0ec729
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-06-21 14:40:57 +0200

    Add News Paper Configuration Example

--- auction-core/src/main/java/io/crowdcode/speedbay/auction/config/NewsPaper.java
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/config/NewsPaper.java
@@ -4,4 +4,10 @@ package io.crowdcode.speedbay.auction.config;
  * @author Ingo Düppe (Crowdcode)
  */
 public class NewsPaper {
+
+    private NewsProvider newsProvider;
+
+    public NewsPaper(NewsProvider newsProvider) {
+        this.newsProvider = newsProvider;
+    }
 }
--- auction-core/src/test/java/io/crowdcode/speedbay/auction/config/NewsConfigurationTest.java
+++ auction-core/src/test/java/io/crowdcode/speedbay/auction/config/NewsConfigurationTest.java
@@ -1,10 +1,33 @@
 package io.crowdcode.speedbay.auction.config;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Ingo Düppe (Crowdcode)
  */
 public class NewsConfigurationTest {
 
+    @Test
+    public void testNewsPaperConfigurationWithXML() throws Exception {
+        String location = "exercise-with-newspaper.xml";
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(location);
+
+        assertNotNull(context.getBean("newsPaper"));
+    }
+
+    @Test
+    public void testNewsPaperConfigurationWithBean() throws Exception {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(NewsConfiguration.class);
+
+        for (String name : context.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+
+        assertNotNull(context.getBean("newsPaper"));
+    }
 }
