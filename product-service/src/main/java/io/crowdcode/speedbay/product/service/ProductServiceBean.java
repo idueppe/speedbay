1c718bc5c8fee21260338a2383874e591b1dc6be
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-06-24 14:04:26 +0200

    WIP

--- product-service/src/main/java/io/crowdcode/speedbay/product/service/ProductServiceBean.java
+++ product-service/src/main/java/io/crowdcode/speedbay/product/service/ProductServiceBean.java
@@ -1,39 +1,44 @@
 package io.crowdcode.speedbay.product.service;
 
 import io.crowdcode.speedbay.product.model.Product;
+import io.crowdcode.speedbay.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Ingo Düppe (Crowdcode)
  */
 @Component
-public class ProductServiceBean {
+public class ProductServiceBean implements ProductService {
 
-    private Map<Long, Product> products;
+    @Autowired
+    private ProductRepository productRepository;
+
 
     public ProductServiceBean() {
-        this.products = new HashMap<>();
     }
 
+    @Override
     public Collection<Product> findAllProducts() {
-        return products.values();
+        return productRepository.findAll();
     }
 
+    @Override
     public Long registerProduct(String name, String description) {
         Product product = new Product()
                 .withId(System.currentTimeMillis())
                 .withName(name)
                 .withDescription(description);
 
-        products.put(product.getId(), product);
+        productRepository.save(product);
+
         return product.getId();
     }
 
+    @Override
     public Product findProduct(Long productId) {
-        return products.get(productId);
+        return productRepository.findOne(productId);
     }
 }
